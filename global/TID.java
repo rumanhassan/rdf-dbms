74738b879ccd4633078a52058a5f2a439f76a542
jahunt1 <jahunt1@asu.edu>
2012-01-27 05:46:43 +0000

    Initial code commit.

---
+++ global/TID.java
@@ -0,0 +1,67 @@
+/*  File TID.java   */
+
+package global;
+
+/** class TID
+ */
+
+public class TID extends java.lang.Object{
+  
+  /** public PageId pageNo
+   */
+  public PageID pageNo = new PageID();
+  
+  /** public int slotNo
+   */
+  public int slotNo;
+  
+  /**
+   * default constructor of class
+   */
+  public TID () { }
+  
+  /**
+   *  constructor of class
+   */
+  public TID (PageID pageno, int slotno)
+    {
+      pageNo = pageno;
+      slotNo = slotno;
+    }
+  
+  /**
+   * make a copy of the given tid
+   */
+  public void copyTid (TID tid)
+    {
+      pageNo = tid.pageNo;
+      slotNo = tid.slotNo;
+    }  
+  
+  /** Compares two TID object, i.e, this to the tid
+   * @param tid TID object to be compared to
+   * @return true is they are equal
+   *         false if not.
+   */
+  public boolean equals(TID tid) {
+    
+    if ((this.pageNo.pid==tid.pageNo.pid)
+	&&(this.slotNo==tid.slotNo))
+      return true;
+    else
+      return false;
+  }
+  
+  /** Write the tid into a byte array at offset
+   * @param ary the specified byte array
+   * @param offset the offset of byte array to write 
+   * @exception java.io.IOException I/O errors
+   */ 
+  public void writeToByteArray(byte [] ary, int offset)
+    throws java.io.IOException
+    {
+      Convert.setIntValue ( slotNo, offset, ary);
+      Convert.setIntValue ( pageNo.pid, offset+4, ary);
+    }
+  
+}
