408cda1f71f4bebbcaf5ac7eb54474e1a183c16f
amit.mascot <amit.mascot@gmail.com>
2012-01-28 22:01:58 +0000

    new EID

---
+++ global/EID.java
@@ -0,0 +1,70 @@
+/*  File EID.java   */
+
+package global;
+
+import java.io.*;
+
+/** class EID
+ */
+
+public class EID{
+  
+  /** public int slotNo
+   */
+  public int slotNo;
+  
+  /** public PageId pageNo
+   */
+  public PageID pageNo = new PageID();
+  
+  /**
+   * default constructor of class
+   */
+  public EID () { }
+  
+  /**
+   *  constructor of class
+   */
+  public EID (PageID pageno, int slotno)
+    {
+      pageNo = pageno;
+      slotNo = slotno;
+    }
+  
+  /**
+   * make a copy of the given rid
+   */
+  public void copyEid (EID eid)
+    {
+      pageNo = eid.pageNo;
+      slotNo = eid.slotNo;
+    }
+  
+  /** Write the rid into a byte array at offset
+   * @param ary the specified byte array
+   * @param offset the offset of byte array to write 
+   * @exception java.io.IOException I/O errors
+   */ 
+  public void writeToByteArray(byte [] ary, int offset)
+    throws java.io.IOException
+    {
+      Convert.setIntValue ( slotNo, offset, ary);
+      Convert.setIntValue ( pageNo.pid, offset+4, ary);
+    }
+  
+  
+  /** Compares two EID object, i.e, this to the eid
+   * @param eid EID object to be compared to
+   * @return true is they are equal
+   *         false if not.
+   */
+  public boolean equals(EID eid) {
+    
+    if ((this.pageNo.pid==eid.pageNo.pid)
+	&&(this.slotNo==eid.slotNo))
+      return true;
+    else
+      return false;
+  }
+  
+}
