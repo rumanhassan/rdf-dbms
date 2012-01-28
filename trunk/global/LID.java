d10e6c4cee9bbb773d974b1449fb62fd3e276406
riteshsukhi109@gmail.com <riteshsukhi109@gmail.com@a78091db-e58d-c8de-a447-0e79fb76489e>
2012-01-28 22:44:50 +0000

    change

--- trunk/global/LID.java
+++ trunk/global/LID.java
@@ -1,5 +1,79 @@
 package global;
 
+import java.io.*;
+/*  File LID.java   */
+/** class LID
+ */
 public class LID {
+/** public int slotNo
+	   */
+	 public int slotNo;
+	  
+	  /** public PageId pageNo
+	   */
+	  public PageId pageNo = new PageId();
+	  
+	  /**
+	   * default constructor of class
+	   */
+	  public LID () { }
+	  
+	  /**
+	   *  constructor of class
+	   */
+	  public LID (PageId pageno, int slotno)
+	    {
+	      pageNo = pageno;
+	      slotNo = slotno;
+	    }
+	  public EID returnEID ()
+	    {
+	      EID eid = new EID(pageNo, slotNo);
+	      
+	      return eid ;
+	    }
+	  public PID returnPID ()
+	    { 
+		  PID pid = new PID( pageNo ,slotNo);
+	      
+	      return pid ;
+	    }
+	  
+	  /**
+	   * make a copy of the given rid
+	   */
+	  public void copyLid (LID lid)
+	    {
+	      pageNo = lid.pageNo;
+	      slotNo = lid.slotNo;
+	    }
+	  
+	  /** Write the lid into a byte array at offset
+	   * @param ary the specified byte array
+	   * @param offset the offset of byte array to write 
+	   * @exception java.io.IOException I/O errors
+	   */ 
+	  public void writeToByteArray(byte [] ary, int offset)
+	    throws java.io.IOException
+	    {
+	      Convert.setIntValue ( slotNo, offset, ary);
+	      Convert.setIntValue ( pageNo.pid, offset+4, ary);
+	    }
+	  
+	  
+	  /** Compares two LID object, i.e, this to the lid
+	   * @param lid LID object to be compared to
+	   * @return true is they are equal
+	   *         false if not.
+	   */
+	  public boolean equals(LID lid) {
+	    
+	    if ((this.pageNo.pid==lid.pageNo.pid)
+		&&(this.slotNo==lid.slotNo))
+	      return true;
+	    else
+	      return false;
+	  }
+	  
+	}
 
-}
