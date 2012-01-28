c2760f8cfacec48eb6120b0ee1b2ae9f3f9cbc10
jahunt1 <jahunt1@asu.edu>
2012-01-28 20:10:38 +0000

    Creation of the tripleheap package. Renamed HeapFile as TripleHeapFile, renamed Tuple as Triple, renamed HFPage as THFPage, renamed Scan as TScan.

---
+++ tripleheap/HFBufMgrException.java
@@ -0,0 +1,20 @@
+package tripleheap;
+import chainexception.*;
+
+public class HFBufMgrException extends ChainException{
+
+
+  public HFBufMgrException()
+  {
+     super();
+  
+  }
+
+  public HFBufMgrException(Exception ex, String name)
+  {
+    super(ex, name);
+  }
+
+
+
+}
