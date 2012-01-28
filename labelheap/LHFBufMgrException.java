c5d9540e9c5026c0fae74a54de30f49be23b4af9
jahunt1 <jahunt1@asu.edu>
2012-01-28 20:55:19 +0000

    Updated classes within the labelheap package.

---
+++ labelheap/LHFBufMgrException.java
@@ -0,0 +1,20 @@
+package labelheap;
+import chainexception.*;
+
+public class LHFBufMgrException extends ChainException{
+
+
+  public LHFBufMgrException()
+  {
+     super();
+  
+  }
+
+  public LHFBufMgrException(Exception ex, String name)
+  {
+    super(ex, name);
+  }
+
+
+
+}
