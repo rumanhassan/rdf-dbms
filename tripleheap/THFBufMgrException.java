e2a8e9432adaefa060e3a91d0fa7b3ede243589f
amit.mascot <amit.mascot@gmail.com>
2012-01-30 01:27:33 +0000

    comit

---
+++ tripleheap/THFBufMgrException.java
@@ -0,0 +1,21 @@
+package tripleheap;
+import chainexception.*;
+
+
+public class THFBufMgrException extends ChainException{
+
+
+  public THFBufMgrException()
+  {
+     super();
+  
+  }
+
+  public THFBufMgrException(Exception ex, String name)
+  {
+    super(ex, name);
+  }
+
+
+
+}
