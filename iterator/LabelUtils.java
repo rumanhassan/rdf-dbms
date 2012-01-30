2e171abd5eed3e61f86afac7ab0bd85c66716b46
jahunt1 <jahunt1@asu.edu>
2012-01-30 03:05:38 +0000

    Updated LabelUtils, 2 methods still need work.

--- iterator/LabelUtils.java
+++ iterator/LabelUtils.java
@@ -10,80 +10,35 @@ import java.lang.*;
  *some useful method when processing Label 
  */
 public class LabelUtils
-{
-  
-  /**
-   * This function compares a label with another label in respective field, and
-   *  returns:
-   *
-   *    0        if the two are equal,
-   *    1        if the tuple is greater,
-   *   -1        if the tuple is smaller,
-   *
-   *@param    fldType   the type of the field being compared.
-   *@param    l1        one tuple.
-   *@param    l2        another tuple.
-   *@param    l1_fld_no the field numbers in the tuples to be compared.
-   *@param    l2_fld_no the field numbers in the tuples to be compared. 
-   *@exception UnknowAttrType don't know the attribute type
-   *@exception IOException some I/O fault
-   *@exception LabelUtilsException exception from this class
-   *@return   0        if the two are equal,
-   *          1        if the tuple is greater,
-   *         -1        if the tuple is smaller,                              
-   */
-  public static int compareLabelWithLabel(AttrType fldType,
-					  Label  l1, int l1_fld_no,
-					  Label  l2, int l2_fld_no)
+{	
+	/**
+	 * This function compares a label with another label in respective field, and
+	 *  returns:
+	 *
+	 *    0        if the two are equal,
+	 *    1        if the tuple is greater,
+	 *   -1        if the tuple is smaller,
+	 *
+	 * @param l1		one label.
+	 * @param l2		another label.
+	 * @return			0        if the two are equal,
+	 *          		1        if the label is greater,
+	 *         		   -1        if the label is smaller,
+	 * @throws IOException some I/O fault
+	 * @throws LabelUtilsException exception from this class
+	 */
+public static int compareLabelWithLabel(Label  l1, Label  l2)
     throws IOException,
-	   UnknowAttrType,
 	   LabelUtilsException
     {
-      int   l1_i,  l2_i;
-      float l1_r,  l2_r;
-      String l1_s, l2_s;
-      
-      switch (fldType.attrType) 
-	{
-	case AttrType.attrInteger:                // Compare two integers.
-	  try {
-	    l1_i = l1.getIntFld(l1_fld_no);
-	    l2_i = l2.getIntFld(l2_fld_no);
-	  }catch (FieldNumberOutOfBoundException e){
-	    throw new LabelUtilsException(e, "FieldNumberOutOfBoundException is caught by LabelUtils.java");
-	  }
-	  if (l1_i == l2_i) return  0;
-	  if (l1_i <  l2_i) return -1;
-	  if (l1_i >  l2_i) return  1;
-	  
-	case AttrType.attrReal:                // Compare two floats
-	  try {
-	    l1_r = l1.getFloFld(l1_fld_no);
-	    l2_r = l2.getFloFld(l2_fld_no);
-	  }catch (FieldNumberOutOfBoundException e){
-	    throw new LabelUtilsException(e, "FieldNumberOutOfBoundException is caught by LabelUtils.java");
-	  }
-	  if (l1_r == l2_r) return  0;
-	  if (l1_r <  l2_r) return -1;
-	  if (l1_r >  l2_r) return  1;
-	  
-	case AttrType.attrString:                // Compare two strings
-	  try {
-	    l1_s = l1.getStrFld(l1_fld_no);
-	    l2_s = l2.getStrFld(l2_fld_no);
-	  }catch (FieldNumberOutOfBoundException e){
-	    throw new LabelUtilsException(e, "FieldNumberOutOfBoundException is caught by LabelUtils.java");
-	  }
+      String l1_s, l2_s;      
+      l1_s = l1.getLabel();
+      l2_s = l2.getLabel();
 	  
 	  // Now handle the special case that is posed by the max_values for strings...
 	  if(l1_s.compareTo( l2_s)>0)return 1;
 	  if (l1_s.compareTo( l2_s)<0)return -1;
-	  return 0;
-	default:
-	  
-	  throw new UnknowAttrType(null, "Don't know how to handle attrSymbol, attrNull");
-	  
-	}
+	  return 0;	
     }
   
   
@@ -92,118 +47,75 @@ public class LabelUtils
    * This function  compares  label1 with another label2 whose
    * field number is same as the label1
    *
-   *@param    fldType   the type of the field being compared.
-   *@param    l1        one tuple
-   *@param    value     another tuple.
-   *@param    l1_fld_no the field numbers in the tuples to be compared.  
+   *@param    l1        one label
+   *@param    value     the string identifier of another label.
    *@return   0        if the two are equal,
-   *          1        if the tuple is greater,
-   *         -1        if the tuple is smaller,  
-   *@exception UnknowAttrType don't know the attribute type   
+   *          1        if the label is greater,
+   *         -1        if the label is smaller,  
    *@exception IOException some I/O fault
    *@exception LabelUtilsException exception from this class   
    */            
-  public static int compareLabelWithValue(AttrType fldType,
-					  Label  l1, int l1_fld_no,
-					  Label  value)
+  public static int compareLabelWithValue(Label  l1, String  value)
     throws IOException,
-	   UnknowAttrType,
 	   LabelUtilsException
     {
-      return compareLabelWithLabel(fldType, l1, l1_fld_no, value, l1_fld_no);
+	  Label tempLabel = new Label();
+	  tempLabel = tempLabel.setLabel(value);
+      return compareLabelWithLabel(l1, tempLabel);
     }
   
   /**
-   *This function Compares two Tuple in all fields 
-   * @param l1 the first tuple
-   * @param l2 the secocnd tuple
+   *This function Compares two Label in all fields 
+   * @param l1 the first label
+   * @param l2 the second label
    * @param type[] the field types
    * @param len the field numbers
    * @return  0        if the two are not equal,
    *          1        if the two are equal,
    *@exception UnknowAttrType don't know the attribute type
    *@exception IOException some I/O fault
-   *@exception TupleUtilsException exception from this class
+   *@exception LabelUtilsException exception from this class
    */            
   
-  public static boolean Equal(Label l1, Label l2, AttrType types[], int len)
-    throws IOException,UnknowAttrType,LabelUtilsException
+  public static boolean Equal(Label l1, Label l2)
+    throws IOException,LabelUtilsException
     {
-      int i;
-      
-      for (i = 1; i <= len; i++)
-	if (compareLabelWithLabel(types[i-1], l1, i, l2, i) != 0)
-	  return false;
+	  if (compareLabelWithLabel(l1, l2) != 0)
+		  return false;
       return true;
     }
   
   /**
    *get the string specified by the field number
    *@param label the Label 
-   *@param fldno the field number
    *@return the content of the field number
    *@exception IOException some I/O fault
    *@exception TupleUtilsException exception from this class
    */
-  public static String Value(Label  label, int fldno)
+  public static String Value(Label  label)
     throws IOException,
 	   TupleUtilsException
     {
       String temp;
-      try{
-	temp = label.getStrFld(fldno);
-      }catch (FieldNumberOutOfBoundException e){
-	throw new LabelUtilsException(e, "FieldNumberOutOfBoundException is caught by LabelUtils.java");
-      }
+      temp = label.getLabel();
       return temp;
     }
   
  
   /**
-   *set up a tuple in specified field from a tuple
+   *set up a label from another label
    *@param value the label to be set 
    *@param label the given label
-   *@param fld_no the field number
-   *@param fldType the tuple attr type
-   *@exception UnknowAttrType don't know the attribute type
    *@exception IOException some I/O fault
-   *@exception TupleUtilsException exception from this class
+   *@exception LabelUtilsException exception from this class
    */  
-  public static void SetValue(Label value, Label  label, int fld_no, AttrType fldType)
+  public static void SetValue(Label value, Label  label)
     throws IOException,
-	   UnknowAttrType,
 	   LabelUtilsException
     {
-      
-      switch (fldType.attrType)
-	{
-	case AttrType.attrInteger:
-	  try {
-	    value.setIntFld(fld_no, label.getIntFld(fld_no));
-	  }catch (FieldNumberOutOfBoundException e){
-	    throw new LabelUtilsException(e, "FieldNumberOutOfBoundException is caught by TupleUtils.java");
-	  }
-	  break;
-	case AttrType.attrReal:
-	  try {
-	    value.setFloFld(fld_no, label.getFloFld(fld_no));
-	  }catch (FieldNumberOutOfBoundException e){
-	    throw new LabelUtilsException(e, "FieldNumberOutOfBoundException is caught by TupleUtils.java");
-	  }
-	  break;
-	case AttrType.attrString:
-	  try {
-	    value.setStrFld(fld_no, label.getStrFld(fld_no));
-	  }catch (FieldNumberOutOfBoundException e){
-	    throw new LabelUtilsException(e, "FieldNumberOutOfBoundException is caught by TupleUtils.java");
-	  }
-	  break;
-	default:
-	  throw new UnknowAttrType(null, "Don't know how to handle attrSymbol, attrNull");
-	  
-	}
-      
-      return;
+	  	// do the stuff we want to do
+	    value.setLabel(label.getLabel());      
+	    return;
     }
   
   
@@ -222,7 +134,8 @@ public class LabelUtils
    *@exception IOException some I/O fault
    *@exception TupleUtilsException exception from this class
    */
-  public static short[] setup_op_tuple(Label Jlabel, AttrType[] res_attrs,
+  /* TODO: uncomment or remove this method
+   * public static short[] setup_op_tuple(Label Jlabel, AttrType[] res_attrs,
 				       AttrType in1[], int len_in1, AttrType in2[], 
 				       int len_in2, short l1_str_sizes[], 
 				       short l2_str_sizes[], 
@@ -275,7 +188,7 @@ public class LabelUtils
 	throw new LabelUtilsException(e,"setHdr() failed");
       }
       return res_str_sizes;
-    }
+    }*/
   
  
    /**
@@ -292,6 +205,7 @@ public class LabelUtils
    *@exception InvalidRelation invalid relation 
    */
 
+  /* TODO: uncomment or remove this method
   public static short[] setup_op_label(Label Jlabel, AttrType res_attrs[],
 				       AttrType in1[], int len_in1,
 				       short l1_str_sizes[], 
@@ -338,7 +252,7 @@ public class LabelUtils
 	throw new LabelUtilsException(e,"setHdr() failed");
       } 
       return res_str_sizes;
-    }
+    } */
 }
 
 
