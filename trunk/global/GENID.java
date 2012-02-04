ccb550859ad2632e962e5767fdd153b335b49a45
riteshsukhi109@gmail.com <riteshsukhi109@gmail.com@a78091db-e58d-c8de-a447-0e79fb76489e>
2012-02-04 00:58:16 +0000

    Genid

--- trunk/global/GENID.java
+++ trunk/global/GENID.java
@@ -1,5 +1,70 @@
+/*  File GENID.java   */
+/* SVN test comment - ok to remove */
+/* comment by Ritesh */
+
 package global;
 
-public class GENID {
+/** class GENID
+ */
 
+public class GENID extends java.lang.Object{
+  
+  /** public PageId pageNo
+   */
+  public PageID pageNo = new PageID();
+  
+  /** public int slotNo
+   */
+  public int slotNo;
+  
+  /**
+   * default constructor of class
+   */
+  public GENID () { }
+  
+  /**
+   *  constructor of class
+   */
+  public GENID (PageID pageno, int slotno)
+    {
+      pageNo = pageno;
+      slotNo = slotno;
+    }
+  
+  /**
+   * make a copy of the given genid
+   */
+  public void copyGenid (GENID genid)
+    {
+      pageNo = genid.pageNo;
+      slotNo = genid.slotNo;
+    }  
+  
+  /** Compares two GENID object, i.e, this to the genid
+   * @param genid GENID object to be compared to
+   * @return true is they are equal
+   *         false if not.
+   */
+  public boolean equals(GENID genid) {
+    
+    if ((this.pageNo.pid==genid.pageNo.pid)
+	&&(this.slotNo==genid.slotNo))
+      return true;
+    else
+      return false;
+  }
+  
+  /** Write the genid into a byte array at offset
+   * @param ary the specified byte array
+   * @param offset the offset of byte array to write 
+   * @exception java.io.IOException I/O errors
+   */ 
+  public void writeToByteArray(byte [] ary, int offset)
+    throws java.io.IOException
+    {
+      Convert.setIntValue ( slotNo, offset, ary);
+      Convert.setIntValue ( pageNo.pid, offset+4, ary);
+    }
+  
 }
+
