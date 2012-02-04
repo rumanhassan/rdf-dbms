bfe5dfe79c59dc38082d03dfc9c54bee208a6b76
jahunt1 <jahunt1@asu.edu>
2012-02-04 08:17:54 +0000

    added some helper methods to support placing a triple in the Stream that matches query criteria.

--- diskmgr/Stream.java
+++ diskmgr/Stream.java
@@ -16,53 +16,39 @@ import tripleheap.*;
  * of the TripleHeapFile.
  */
 public class Stream implements GlobalConst{
+	// some values to avoid magic numbers
+	final int UNINITIALIZED = -1; 
+	final int NOT_INCLUDED = 0; 
+	final int INCLUDED = 1; 
+	final int SUBJIDX = 0;
+	final int PREDIDX = 1;
+	final int OBJIDX = 2;
+	final int CONFIDX = 3;
 	
 	// attributes of the Stream
-	private DB rdfdatabase;
+	private rdfDB rdfdatabase;
 	private int orderType;
 	private String subjectFilter;
 	private String predicateFilter;
 	private String objectFilter;
-	private double confidenceFilter;
-
-    /** The TripleHeapFile we are using. */
-    private TripleHeapFile  _thf;
-
-    /** PageId of current directory page (which is itself a THFPage) */
-    private PageID dirpageId = new PageID();
-
-    /** pointer to in-core data of dirpageId (page is pinned) */
-    private THFPage dirpage = new THFPage();
-
-    /** record ID of the DataPageInfo struct (in the directory page) which
-     * describes the data page where our current record lives.
-     */
-    private TID datapageTid = new TID();
-
-    /** the actual PageId of the data page with the current record */
-    private PageID datapageId = new PageID();
-
-    /** in-core copy (pinned) of the same */
-    private THFPage datapage = new THFPage();
-
-    /** record ID of the current record (from the current data page) */
-    private TID usertid = new TID();
-
-    /** Status of next user status */
-    private boolean nextUserStatus;
-    
+	private float confidenceFilter;
+	private short[] filtersIncluded = {UNINITIALIZED, UNINITIALIZED, 
+			UNINITIALIZED, UNINITIALIZED};
      
-    /** The constructor pins the first directory page in the file
-     * and initializes its private data members from the private
-     * data member from hf
+    /** The constructor initializes the Stream's private data members from the
+     * given parameters.
      *
-     * @exception InvalidTripleSizeException Invalid triple size
-     * @exception IOException I/O errors
-     *
-     * @param thf A TripleHeapFile object
-     */
-  public Stream(DB rdfDataBase, int orderType, String subjectFilter, 
-		 String predicateFilter, String objectFilter, double confidenceFilter)
+	 * @param rdfDataBase The rdfDB used to access heap files/b-trees
+	 * @param orderType
+	 * @param subjectFilter
+	 * @param predicateFilter
+	 * @param objectFilter
+	 * @param confidenceFilter
+	 * @throws InvalidTripleSizeException Invalid triple size
+	 * @throws IOException I/O errors
+	 */
+	public Stream(rdfDB rdfDataBase, int orderType, String subjectFilter, 
+		 String predicateFilter, String objectFilter, float confidenceFilter)
     throws InvalidTripleSizeException,
 	   	   IOException
   {
@@ -72,9 +58,8 @@ public class Stream implements GlobalConst{
 	  this.predicateFilter = predicateFilter;
 	  this.objectFilter = objectFilter;
 	  this.confidenceFilter = confidenceFilter;
+	  processFilters(subjectFilter, predicateFilter,objectFilter, confidenceFilter);
   }
-
-
   
   /** Retrieve the next triple in the stream
    *
@@ -82,7 +67,8 @@ public class Stream implements GlobalConst{
    * @exception IOException I/O errors
    *
    * @param tid Triple ID of the triple
-   * @return the Triple object with the specified TID.
+   * @return the Triple object with the specified TID. If no such triple 
+   * 		exists, return null.
    */
   public Triple getNext(TID tid) 
     throws InvalidTripleSizeException,
@@ -90,550 +76,80 @@ public class Stream implements GlobalConst{
   {
     Triple recptrtriple = null;
     
-    if (nextUserStatus != true) {
-        nextDataPage();
-    }
-     
-    if (datapage == null)
-      return null;
-    
-    tid.pageNo.pid = usertid.pageNo.pid;    
-    tid.slotNo = usertid.slotNo;
-         
-    try {
-      recptrtriple = datapage.getTriple(tid);
-    }
-    
-    catch (Exception e) {
-      System.err.println("STREAM: Error in Stream" + e);
-      e.printStackTrace();
-    }   
-    
-    usertid = datapage.nextTriple(tid);
-    if(usertid == null) nextUserStatus = false;
-    else nextUserStatus = true;
+    // TODO Write the code to perform the following actions:
+    // 1. Use rdfDataBase's b-tree structure to get the next Triple from the
+    //    triple heap file.    
+    // 2. Check that the triple's data matches the filter
+    // 3. Return the triple with matching TID
      
     return recptrtriple;
-  }
-
-
-    /** Position the Stream cursor to the record with the given tid.
-     * 
-     * @exception InvalidTripleSizeException Invalid triple size
-     * @exception IOException I/O errors
-     * @param tid Triple ID of the given triple
-     * @return 	true if successful, 
-     *			false otherwise.
-     */
-  public boolean position(TID tid) 
-    throws InvalidTripleSizeException,
-	   IOException
-  { 
-    TID    nxtTid = new TID();
-    boolean bst;
-
-    bst = peekNext(nxtTid);
-
-    if (nxtTid.equals(tid)==true) 
-    	return true;
-
-    // This is kind lame, but otherwise it will take all day.
-    PageID pgid = new PageID();
-    pgid.pid = tid.pageNo.pid;
- 
-    if (!datapageId.equals(pgid)) {
-
-      // reset everything and start over from the beginning
-      reset();
-      
-      bst =  firstDataPage();
-
-      if (bst != true)
-	return bst;
-      
-      while (!datapageId.equals(pgid)) {
-	bst = nextDataPage();
-	if (bst != true)
-	  return bst;
-      }
-    }
-    
-    // Now we are on the correct page.
-    
-    try{
-    	usertid = datapage.firstTriple();
-	}
-    catch (Exception e) {
-      e.printStackTrace();
-    }
-	
-
-    if (usertid == null)
-      {
-    	bst = false;
-        return bst;
-      }
-    
-    bst = peekNext(nxtTid);
-    
-    while ((bst == true) && (nxtTid != tid))
-      bst = mvNext(nxtTid);
-    
-    return bst;
   }
 
     /** Closes the Stream object */
     public void closeStream()
     {
-    	reset();
+    	// TODO Write code to perform cleanup
     }
-   
-
-    /** Reset everything and unpin all pages. */
-    private void reset()
-    { 
-
-    if (datapage != null) {
     
-    try{
-      unpinPage(datapageId, false);
-    }
-    catch (Exception e){
-       	System.err.println("STREAM: Error in Stream" + e);
-      e.printStackTrace();
-    }  
-    }
-    datapageId.pid = 0;
-    datapage = null;
-
-    if (dirpage != null) {
-    
-      try{
-	unpinPage(dirpageId, false);
-      }
-      catch (Exception e){
-	     System.err.println("STREAM: Error in Stream: " + e);
-	e.printStackTrace();
-      }
-    }
-    dirpage = null;
- 
-    nextUserStatus = true;
-
-  }
- 
- 
-  /** Move to the first data page in the file. 
-   * @exception InvalidTripleSizeException Invalid tuple size
-   * @exception IOException I/O errors
-   * @return true if successful
-   *         false otherwise
-   */
-  private boolean firstDataPage() 
-    throws InvalidTripleSizeException,
-	   IOException
-  {
-    DataPageInfo dpinfo;
-    Triple        rectuple = null;
-    Boolean      bst;
-
-    /** copy data about first directory page */
- 
-    dirpageId.pid = _thf._firstDirPageId.pid;  
-    nextUserStatus = true;
-
-    /** get first directory page and pin it */
-    	try {
-	   dirpage  = new THFPage();
-       	   pinPage(dirpageId, (Page) dirpage, false);	   
-       }
-
-    	catch (Exception e) {
-    //    System.err.println("SCAN Error, try pinpage: " + e);
-	e.printStackTrace();
-	}
-    
-    /** now try to get a pointer to the first datapage */
-	 datapageTid = dirpage.firstTriple();
-	 
-    	if (datapageTid != null) {
-    /** there is a datapage record on the first directory page: */
-	
-	try {
-          rectuple = dirpage.getTriple(datapageTid);
-	}  
-				
-	catch (Exception e) {
-	//	System.err.println("SCAN: Chain Error in Scan: " + e);
-		e.printStackTrace();
-	}		
-      			    
-    	dpinfo = new DataPageInfo(rectuple);
-        datapageId.pid = dpinfo.pageId.pid;
-
-    } else {
-
-    /** the first directory page is the only one which can possibly remain
-     * empty: therefore try to get the next directory page and
-     * check it. The next one has to contain a datapage record, unless
-     * the tripleheapfile is empty:
+    /** Sets the filtersIncluded array to keep track of which filters are used
+     * @param subj subject filter
+     * @param pred predicate filter
+     * @param obj object filter
+     * @param conf confidence filter
      */
-      PageID nextDirPageId = new PageID();
-      
-      nextDirPageId = dirpage.getNextPage();
-      
-      if (nextDirPageId.pid != INVALID_PAGE) {
-	
-	try {
-            unpinPage(dirpageId, false);
-            dirpage = null;
-	    }
-	
-	catch (Exception e) {
-	//	System.err.println("SCAN: Error in 1stdatapage 1 " + e);
-		e.printStackTrace();
-	}
-        	
-	try {
-	
-           dirpage = new THFPage();
-	    pinPage(nextDirPageId, (Page )dirpage, false);
-	
-	    }
-	
-	catch (Exception e) {
-	//  System.err.println("SCAN: Error in 1stdatapage 2 " + e);
-	  e.printStackTrace();
-	}
-	
-	/** now try again to read a data record: */
-	
-	try {
-	  datapageTid = dirpage.firstTriple();
-	}
-        
-	catch (Exception e) {
-	//  System.err.println("SCAN: Error in 1stdatapg 3 " + e);
-	  e.printStackTrace();
-	  datapageId.pid = INVALID_PAGE;
-	}
-       
-	if(datapageTid != null) {
-          
-	  try {
-	  
-	    rectuple = dirpage.getTriple(datapageTid);
-	  }
-	  
-	  catch (Exception e) {
-	//    System.err.println("SCAN: Error getTriple 4: " + e);
-	    e.printStackTrace();
-	  }
-	  
-	  if (rectuple.getLength() != DataPageInfo.size)
-	    return false;
-	  
-	  dpinfo = new DataPageInfo(rectuple);
-	  datapageId.pid = dpinfo.pageId.pid;
-	  
-         } else {
-	   // heapfile empty
-           datapageId.pid = INVALID_PAGE;
-         }
-       }//end if01
-       else {// heapfile empty
-	datapageId.pid = INVALID_PAGE;
-	}
-}	
-	
-	datapage = null;
-
-	try{
-         nextDataPage();
-	  }
-	  
-	catch (Exception e) {
-	//  System.err.println("SCAN Error: 1st_next 0: " + e);
-	  e.printStackTrace();
-	}
-	
-      return true;
-      
-      /** ASSERTIONS:
-       * - first directory page pinned
-       * - this->dirpageId has Id of first directory page
-       * - this->dirpage valid
-       * - if heapfile empty:
-       *    - this->datapage == NULL, this->datapageId==INVALID_PAGE
-       * - if heapfile nonempty:
-       *    - this->datapage == NULL, this->datapageId, this->datapageRid valid
-       *    - first datapage is not yet pinned
-       */
-    
-  }
-    
-
-  /** Move to the next data page in the file and 
-   * retrieve the next data page. 
-   *
-   * @return 		true if successful
-   *			false if unsuccessful
-   */
-  private boolean nextDataPage() 
-    throws InvalidTripleSizeException,
-	   IOException
-  {
-    DataPageInfo dpinfo;
-    
-    boolean nextDataPageStatus;
-    PageID nextDirPageId = new PageID();
-    Triple rectuple = null;
-
-  // ASSERTIONS:
-  // - this->dirpageId has Id of current directory page
-  // - this->dirpage is valid and pinned
-  // (1) if heapfile empty:
-  //    - this->datapage==NULL; this->datapageId == INVALID_PAGE
-  // (2) if overall first record in heapfile:
-  //    - this->datapage==NULL, but this->datapageId valid
-  //    - this->datapageRid valid
-  //    - current data page unpinned !!!
-  // (3) if somewhere in heapfile
-  //    - this->datapageId, this->datapage, this->datapageRid valid
-  //    - current data page pinned
-  // (4)- if the scan had already been done,
-  //        dirpage = NULL;  datapageId = INVALID_PAGE
-    
-    if ((dirpage == null) && (datapageId.pid == INVALID_PAGE))
-        return false;
-
-    if (datapage == null) {
-      if (datapageId.pid == INVALID_PAGE) {
-	// heapfile is empty to begin with
-	
-	try{
-	  unpinPage(dirpageId, false);
-	  dirpage = null;
-	}
-	catch (Exception e){
-	//  System.err.println("Scan: Chain Error: " + e);
-	  e.printStackTrace();
-	}
-	
-      } else {
-	
-	// pin first data page
-	try {
-	  datapage  = new THFPage();
-	  pinPage(datapageId, (Page) datapage, false);
-	}
-	catch (Exception e){
-	  e.printStackTrace();
-	}
-	
-	try {
-	  usertid = datapage.firstTriple();
-	}
-	catch (Exception e) {
-	  e.printStackTrace();
-	}
-	
-	return true;
-        }
-    }
-  
-  // ASSERTIONS:
-  // - this->datapage, this->datapageId, this->datapageRid valid
-  // - current datapage pinned
-
-    // unpin the current datapage
-    try{
-      unpinPage(datapageId, false /* no dirty */);
-        datapage = null;
-    }
-    catch (Exception e){
-      
-    }
-          
-    // read next datapagerecord from current directory page
-    // dirpage is set to NULL at the end of scan. Hence
-    
-    if (dirpage == null) {
-      return false;
+    private void processFilters(String subj, String pred, String obj, 
+    		float conf){
+    	// subject filter null or 0
+    	if( (subj == null) || subj.equalsIgnoreCase("0"))
+    		filtersIncluded[SUBJIDX] = NOT_INCLUDED;
+    	else filtersIncluded[SUBJIDX] = INCLUDED;
+    	// predicate filter null or 0
+    	if( (pred == null) || pred.equalsIgnoreCase("0"))
+    		filtersIncluded[PREDIDX] = NOT_INCLUDED;
+    	else filtersIncluded[PREDIDX] = INCLUDED;
+    	// object filter null or 0
+    	if( (obj == null) || obj.equalsIgnoreCase("0"))
+    		filtersIncluded[OBJIDX] = NOT_INCLUDED;
+    	else filtersIncluded[OBJIDX] = INCLUDED;
+    	
+    	float zero = 0;
+        int i1 = Float.compare(zero,conf);
+    	if( i1 == 0) // confidence filter equals 0
+    		filtersIncluded[CONFIDX] = NOT_INCLUDED;
+    	else filtersIncluded[CONFIDX] = INCLUDED;    	
     }
     
-    datapageTid = dirpage.nextTriple(datapageTid);
-    
-    if (datapageTid == null) {
-      nextDataPageStatus = false;
-      // we have read all datapage records on the current directory page
-      
-      // get next directory page
-      nextDirPageId = dirpage.getNextPage();
-  
-      // unpin the current directory page
-      try {
-	unpinPage(dirpageId, false /* not dirty */);
-	dirpage = null;
-	
-	datapageId.pid = INVALID_PAGE;
-      }
-      
-      catch (Exception e) {
-	
-      }
-		    
-      if (nextDirPageId.pid == INVALID_PAGE)
-	return false;
-      else {
-	// ASSERTION:
-	// - nextDirPageId has correct id of the page which is to get
-	
-	dirpageId = nextDirPageId;
-	
- 	try { 
-	  dirpage  = new THFPage();
-	  pinPage(dirpageId, (Page)dirpage, false);
-	}
-	
-	catch (Exception e){
-	  
-	}
-	
-	if (dirpage == null)
-	  return false;
-	
-    	try {
-	  datapageTid = dirpage.firstTriple();
-	  nextDataPageStatus = true;
-	}
-	catch (Exception e){
-	  nextDataPageStatus = false;
-	  return false;
-	} 
-      }
+    /** Checks if a triple's data matches the filter criteria
+     * @param subject
+     * @param predicate
+     * @param object
+     * @param confidence
+     * @return true if there is a match, false otherwise
+     */
+    public boolean tripleDataMatchesFilter(String subject, String predicate,
+    		String object, float confidence){
+    	boolean matches = false;
+    	
+    	// subject filter matches or is not considered
+    	if( filtersIncluded[SUBJIDX] == NOT_INCLUDED ||
+    		(filtersIncluded[SUBJIDX] == INCLUDED && subject.equalsIgnoreCase(subjectFilter) ) ){
+    		// predicate filter matches or is not considered
+    		if( filtersIncluded[PREDIDX] == NOT_INCLUDED ||
+    	    	(filtersIncluded[PREDIDX] == INCLUDED && predicate.equalsIgnoreCase(predicateFilter) ) ){
+    			// object filter matches or is not considered
+    			if( filtersIncluded[OBJIDX] == NOT_INCLUDED ||
+    	    	    (filtersIncluded[OBJIDX] == INCLUDED && object.equalsIgnoreCase(objectFilter) ) ){
+    				// confidence is greater than or equal to the filter
+    				if( Float.compare(confidenceFilter,confidence) >= 0 ){
+    					// if you got to this point, all criteria are met
+    					matches = true;
+    				}
+    			}    			
+    		}
+    	}
+    	
+    	return matches;    	
     }
     
-    // ASSERTION:
-    // - this->dirpageId, this->dirpage valid
-    // - this->dirpage pinned
-    // - the new datapage to be read is on dirpage
-    // - this->datapageRid has the Rid of the next datapage to be read
-    // - this->datapage, this->datapageId invalid
-  
-    // data page is not yet loaded: read its record from the directory page
-   	try {
-	  rectuple = dirpage.getTriple(datapageTid);
-	}
-	
-	catch (Exception e) {
-	  System.err.println("HeapFile: Error in Scan" + e);
-	}
-	
-	if (rectuple.getLength() != DataPageInfo.size)
-	  return false;
-                        
-	dpinfo = new DataPageInfo(rectuple);
-	datapageId.pid = dpinfo.pageId.pid;
-	
- 	try {
-	  datapage = new THFPage();
-	  pinPage(dpinfo.pageId, (Page) datapage, false);
-	}
-	
-	catch (Exception e) {
-	  System.err.println("HeapFile: Error in Scan" + e);
-	}
-	
-     
-     // - directory page is pinned
-     // - datapage is pinned
-     // - this->dirpageId, this->dirpage correct
-     // - this->datapageId, this->datapage, this->datapageRid correct
-
-     usertid = datapage.firstTriple();
-     
-     if(usertid == null)
-     {
-       nextUserStatus = false;
-       return false;
-     }
-  
-     return true;
-  }
-
-
-  private boolean peekNext(TID rid) {
-    
-    rid.pageNo.pid = usertid.pageNo.pid;
-    rid.slotNo = usertid.slotNo;
-    return true;
-    
-  }
-
-
-  /** Move to the next record in a sequential scan.
-   * Also returns the TID of the (new) current record.
-   */
-  private boolean mvNext(TID tid) 
-    throws InvalidTripleSizeException,
-	   IOException
-  {
-    TID nextrid;
-    boolean status;
-
-    if (datapage == null)
-        return false;
-
-    	nextrid = datapage.nextTriple(tid);
-	
-	if( nextrid != null ){
-	  usertid.pageNo.pid = nextrid.pageNo.pid;
-	  usertid.slotNo = nextrid.slotNo;
-	  return true;
-	} else {
-	  
-	  status = nextDataPage();
-
-	  if (status==true){
-	    tid.pageNo.pid = usertid.pageNo.pid;
-	    tid.slotNo = usertid.slotNo;
-	  }
-	
-	}
-	return true;
-  }
-
-    /**
-   * short cut to access the pinPage function in bufmgr package.
-   * @see bufmgr.pinPage
-   */
-  private void pinPage(PageID pageno, Page page, boolean emptyPage)
-    throws THFBufMgrException {
-
-    try {
-      SystemDefs.JavabaseBM.pinPage(pageno, page, emptyPage);
-    }
-    catch (Exception e) {
-      throw new THFBufMgrException(e,"Scan.java: pinPage() failed");
-    }
-
-  } // end of pinPage
-
-  /**
-   * short cut to access the unpinPage function in bufmgr package.
-   * @see bufmgr.unpinPage
-   */
-  private void unpinPage(PageID pageno, boolean dirty)
-    throws THFBufMgrException {
-
-    try {
-      SystemDefs.JavabaseBM.unpinPage(pageno, dirty);
-    }
-    catch (Exception e) {
-      throw new THFBufMgrException(e,"Scan.java: unpinPage() failed");
-    }
-
-  } // end of unpinPage
   
 } // end of class Stream
