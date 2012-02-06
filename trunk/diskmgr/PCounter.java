9249fa7abb49525cc68bb370e16e39227df5c3ae
riteshsukhi109@gmail.com <riteshsukhi109@gmail.com@a78091db-e58d-c8de-a447-0e79fb76489e>
2012-02-06 01:31:27 +0000

    comment

--- trunk/diskmgr/PCounter.java
+++ trunk/diskmgr/PCounter.java
@@ -2,13 +2,22 @@ package diskmgr;
 public class PCounter {
 public static int rcounter;
 public static int wcounter;
+/**
+ * Initializes the rcounter and wcounter to keep the record of number of disk pages read.
+ */
 public static void initialize() {
 rcounter =0;
 wcounter =0;
 }
+/**
+ * Increment the read counter
+ */
 public static void readIncrement() {
 rcounter++;
 }
+/**
+ * Increment the write counter
+ */
 public static void writeIncrement() {
 wcounter++;
 }
