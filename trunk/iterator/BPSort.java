18deeba2e84e62e9c21aa7c66a6994702252acab
amit.mascot@gmail.com <amit.mascot@gmail.com@a78091db-e58d-c8de-a447-0e79fb76489e>
2012-02-19 00:54:51 +0000

    working sort

--- trunk/iterator/BPSort.java
+++ trunk/iterator/BPSort.java
@@ -7,12 +7,13 @@ import global.PageID;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 import labelheap.InvalidLabelSizeException;
 import labelheap.LabelHeapFile;
 import tripleheap.BasicPatternClass;
 
-public class BPSort {
+public class BPSort /* implements Comparator */{
 
 	/**
 	 * @param input_itr
@@ -23,19 +24,21 @@ public class BPSort {
 	 * @throws InvalidLabelSizeException
 	 * @throws labelheap.InvalidSlotNumberException
 	 */
+	@SuppressWarnings("unchecked")
 	public BPSort(BPIterator input_itr, BPOrder returnEiD, int SortNodeIDPos,
 			int n_pages) throws labelheap.InvalidSlotNumberException,
 			InvalidLabelSizeException, Exception {
-		@SuppressWarnings("unchecked")
-		ArrayList<BasicPatternClass> al = input_itr.getArrayList();
-		java.util.Iterator<BasicPatternClass> bpIterator = al.listIterator();
+		java.util.Iterator<BasicPatternClass> bpIterator = input_itr
+				.getArrayList().listIterator();
 		LabelHeapFile entlabelfileObj = new LabelHeapFile("file_2");
 		EID eid = new EID();
 		LID lid = new LID();
 		PageID pageID = new PageID();
 		ArrayList<String> b1 = new ArrayList<String>();
-		while (bpIterator.hasNext()) {
-			eid = bpIterator.next().getEIDbyNodePosition(SortNodeIDPos);
+		BasicPatternClass bpc_temp = new BasicPatternClass();
+		while ((bpIterator.hasNext())) {
+			bpc_temp = bpIterator.next();
+			eid = bpc_temp.getEIDbyNodePosition(SortNodeIDPos);
 			pageID.pid = eid.pageNo.pid;
 			lid.pageNo = pageID;
 			lid.slotNo = eid.slotNo;
@@ -43,26 +46,37 @@ public class BPSort {
 			b1.add(label);
 		}
 
-		Collections.sort(b1);
+		bpIterator = input_itr.getArrayList().listIterator();
+		Collections.sort(b1, new sort1());
 		int blength;
 		blength = b1.size();
+		System.out.print("Sorted Patterns \n");
 		for (int i = 0; i < blength; i++) {
 			String compareLabel = b1.get(i);
 			while (bpIterator.hasNext()) {
-				eid = bpIterator.next().getEIDbyNodePosition(SortNodeIDPos);
+				bpc_temp = bpIterator.next();
+				eid = bpc_temp.getEIDbyNodePosition(SortNodeIDPos);
 				pageID.pid = eid.pageNo.pid;
 				lid.pageNo = pageID;
 				lid.slotNo = eid.slotNo;
 				String label = entlabelfileObj.getLabel(lid);
 				if (compareLabel.equalsIgnoreCase(label)) {
-					String[] basicPatternStrings = bpIterator.next()
-							.convertIdsToStrings();
-					for (int j = 0; i < basicPatternStrings.length; j++) {
-						System.out.print(basicPatternStrings[j] + "");
-					}
-					System.out.print(bpIterator.next().getConfidence() + "\n");
+					System.out.print("BP " + i+ ": ");
+					bpc_temp.print();
 				}
 			}
+			bpIterator = input_itr.getArrayList().listIterator();
 		}
-	}	
+	}
+}
+
+@SuppressWarnings("rawtypes")
+class sort1 implements Comparator {
+	@Override
+	public int compare(Object o1, Object o2) {
+		String s1 = (String) o1;
+		String s2 = (String) o2;
+
+		return s1.toLowerCase().compareTo(s2.toLowerCase());
+	}
 }
